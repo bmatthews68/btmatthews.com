d17ff0dd211f688a720d100928c69f492c4edd1a
Brian Matthews <brian@btmatthews.com>
2013-03-07 21:24:09 +0000

    Updating content

--- webspace/httpdocs/index.php
+++ webspace/httpdocs/index.php
@@ -12,25 +12,32 @@
       	  solutions on the Java, .NET, Windows and Unix technology stacks in the Java and C/C++ programming languages
       	  using proprietary and open source technologies.</p>
       	<p>
-      		<a href="http://www.newbay.com">Newbay</a>, 
-      		<a href="http://www.leaseplanis.com">LeasePlan Information Services</a>, 
-      		<a href="http://www.dnb.com">D&amp;B Business Information Services</a>, 
-      		<a href="http://www.fujitsu.com/ie/services/">Fujitsu Services</a>, 
-      		<a href="http://www.corvil.net">Corvil</a>, 
-      		<a href="http://www.vodafone.com">Vodafone</a>, 
-      		<a href="http://www.terranua.com">TerraNua</a>,
-			<a href="http://www.fidelity.com">Fidelity Investements</a>, 
-			<a href="http://www.kvh.co.jp/en">KVH</a>, 
-			International Financial Systems,
-      		<a href="http://www.att.com">AT&amp;T</a>, 
-      		<a href="http://www.ibm.com">IBM</a>, 
+      		As a contractor at <a href="http://www.newbay.com">Newbay</a> I developed the Mac OS X client for
+      		fo SyncDrive. At <a href="http://www.corvil.net">Corvil</a> I developed decoders for market data feeds,
+      		trading protocols and 
+      		While working with <a href="http://www.vodafone.com">Vodafone</a>, 
+			I spent two years on secondment to <a href="http://www.kvh.co.jp/en">KVH</a> in Tokyo as the Lead System Architect
+			integrating operations and business support systems. 
+			I contacted with <a href="http://www.att.com">AT&amp;T</a> on two separate occassions. During my first assignment
+			I developed an the object request broker and on the second assignment integrated operational support systems.
+      		On my first assignment at <a href="http://www.ibm.com">IBM</a> I helped get an ETL tool called data refresher to marke
+      	t
+      	second assignmet, 
       		<a href="http://www.microsfot.com">Microsoft</a>, 
       		Lotus Development, 
-      		Lehman Brothers, 
       		<a href="http://www.hp.com">Hewlett-Packard</a>, 
+ 
+      		<a href="http://www.leaseplanis.com">LeasePlan Information Services</a>, 
+      		<a href="http://www.dnb.com">D&amp;B Business Information Services</a>, 
+      		<a href="http://www.terranua.com">TerraNua</a>,
+			<a href="http://www.fidelity.com">Fidelity Investements</a>, 
+			International Financial Systems,
+      		Lehman Brothers, 
+
+      		<a href="http://www.fujitsu.com/ie/services/">Fujitsu Services</a>, 
       		British Rail and 
       		Polydata.</p>
-        <p>I graduated with <a href="http://www.dcu.ie/prospective/deginfo.php?classname=CA">B.Sc. in Computer Applications</a>
+        <p>I graduated with a <a href="http://www.dcu.ie/prospective/deginfo.php?classname=CA">B.Sc. in Computer Applications</a>
           from <a href="http://dcu.ie">Dublin City University</a> in 1990.</p>
       </div>
     </div>
